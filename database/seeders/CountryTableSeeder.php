47ab009b42a927189824cbd82bae21606f7cd414
jabbar9141 <ab.jabbar9141@gmail.com>
2024-09-08 21:48:59 +0000

    new chages

--- database/seeders/CountryTableSeeder.php
+++ database/seeders/CountryTableSeeder.php
@@ -14,33 +14,33 @@ class CountryTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CountriesTableSeeder::class);
-        $this->call(StatesTableSeeder::class);
-        $this->call(CitiesTableChunkOneSeeder::class);
-        $this->call(CitiesTableChunkTwoSeeder::class);
-        $this->call(CitiesTableChunkThreeSeeder::class);
+        // $this->call(CountriesTableSeeder::class);
+        // $this->call(StatesTableSeeder::class);
+        // $this->call(CitiesTableChunkOneSeeder::class);
+        // $this->call(CitiesTableChunkTwoSeeder::class);
+        // $this->call(CitiesTableChunkThreeSeeder::class);
         $this->call(CitiesTableChunkFourSeeder::class);
         $this->call(CitiesTableChunkFiveSeeder::class);
         $this->call(CitiesChunkSixSeeder::class);
-        // $this->call(CitiesTableChunkSevenSeeder::class);
+        $this->call(CitiesTableChunkSevenSeeder::class);
 
         // if (DB::connection()->getName() == 'pgsql') {
-            $tablesToCheck = ['countries', 'states', 'cities'];
-            foreach ($tablesToCheck as $tableToCheck) {
-                // $this->command->info('Checking the next id sequence for ' . $tableToCheck);
-                $highestId = DB::table($tableToCheck)->select(DB::raw('MAX(id)'))->first();
-                $nextId = DB::table($tableToCheck)->select(DB::raw('nextval(\''.$tableToCheck.'_id_seq\')'))->first();
-                if ($nextId->nextval < $highestId->max) {
-                    DB::select('SELECT setval(\''.$tableToCheck.'_id_seq\', '.$highestId->max.')');
-                    $highestId = DB::table($tableToCheck)->select(DB::raw('MAX(id)'))->first();
-                    $nextId = DB::table($tableToCheck)->select(DB::raw('nextval(\''.$tableToCheck.'_id_seq\')'))->first();
-                    if ($nextId->nextval > $highestId->max) {
-                        // $this->command->info($tableToCheck . ' autoincrement corrected');
-                    } else {
-                        // $this->command->info('Arff! The nextval sequence is still all screwed up on ' . $tableToCheck);
-                    }
-                }
-            }
+            // $tablesToCheck = ['countries', 'states', 'cities'];
+            // foreach ($tablesToCheck as $tableToCheck) {
+            //     // $this->command->info('Checking the next id sequence for ' . $tableToCheck);
+            //     $highestId = DB::table($tableToCheck)->select(DB::raw('MAX(id)'))->first();
+            //     $nextId = DB::table($tableToCheck)->select(DB::raw('nextval(\''.$tableToCheck.'_id_seq\')'))->first();
+            //     if ($nextId->nextval < $highestId->max) {
+            //         DB::select('SELECT setval(\''.$tableToCheck.'_id_seq\', '.$highestId->max.')');
+            //         $highestId = DB::table($tableToCheck)->select(DB::raw('MAX(id)'))->first();
+            //         $nextId = DB::table($tableToCheck)->select(DB::raw('nextval(\''.$tableToCheck.'_id_seq\')'))->first();
+            //         if ($nextId->nextval > $highestId->max) {
+            //             // $this->command->info($tableToCheck . ' autoincrement corrected');
+            //         } else {
+            //             // $this->command->info('Arff! The nextval sequence is still all screwed up on ' . $tableToCheck);
+            //         }
+            //     }
+            // }
 
         // }
     }
